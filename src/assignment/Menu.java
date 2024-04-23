dd10b76c45811f72801b95dbb226ef55ba31a70c
KYUNS <Hp@LAPTOP-85A8NHHE>
2024-04-23 12:41:15 +0800

    put together

---
+++ src/assignment/Menu.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment;
+
+/**
+ *
+ * @author Hp
+ */
+public class Menu {
+    public static void splashScreen(){
+        System.out.println("WELCOME TO BEEWARD!");
+        
+    }
+    public static void mainMenu(){
+        System.out.print("1.Register New Account\n2.LOGIN TO EXISTING ACCOUNT\n3.Exit\nEnter Your Choice: ");
+    }
+    public static void userMenu(){
+        System.out.print("1.Update Details\n2.Add New Transaction Amount\n3.Redeem Point\n4.Back\nEnter choice: ");
+    }
+    public static void backAction(){
+        System.out.println("\n===========GOING BACK==========\n");
+    }
+   
+    
+}

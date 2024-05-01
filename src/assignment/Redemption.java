0b69a1883ef1fd7f426d558ef23399359bbbb592
Asus <Asus@LAPTOP-GC1TDGI5>
2024-05-01 15:30:17 +0800

    Updated

--- src/assignment/Redemption.java
+++ src/assignment/Redemption.java
@@ -98,146 +98,19 @@ public class Redemption {
     
     //PRODUCT   
     
-public class Product {
-    private String[] productName;
-    private String[] prodDescription;
-    private static int[] qtyRemaining;
-    private int[] requiredPoint;
 
-    public Product() {
-    }
-
-    public Product(String[] productName, String[] prodDescription, int[] qtyRemaining, int[] requiredPoint) {
-        this.productName = productName;
-        this.prodDescription = prodDescription;
-        this.qtyRemaining = qtyRemaining;
-        this.requiredPoint = requiredPoint;
-    }
-
-    public String getProductName(int i) {
-        return productName[i];
-    }
-
-    public void setProductName(String[] productName) {
-        this.productName = productName;
-    }
-
-    public String getProdDescription(int i) {
-        return prodDescription[i];
-    }
-
-    public void setProdDescription(String[] productDescription) {
-        this.prodDescription = productDescription;
-    }
-
-    public static int getQtyRemaining(int i) {
-        return qtyRemaining[i];
-    }
-
-    public static void setQtyRemaining(int[] qtyRemaining) {
-        Product.qtyRemaining = qtyRemaining;
-    }
-
-    public int getRequiredPoint(int i) {
-        return requiredPoint[i];
-    }
-
-    public void setRequiredPoint(int[] requiredPoint) {
-        this.requiredPoint = requiredPoint;
-    }
-    
-    public void displayProduct(){
-        System.out.print("");
-    }
-}
-    
+   
     //REDEMPTION PRODUCT
     
-public class RedemptionProduct extends Product{
 
-    public RedemptionProduct() {
-    }
-
-    public RedemptionProduct(String[] productName, String[] productDescription, int[] qtyRemaining, int[] requiredPoint) {
-        super(productName, productDescription, qtyRemaining, requiredPoint);
-    }
-}
     
     //LIMITED PRODUCT
     
-public class LimitedProduct extends Product{
-    private String[] tier;
 
-    public LimitedProduct() {
-    }
-
-    public LimitedProduct(String[] tier, String[] productName, String[] prodDescription, int[] productQty, int[] qtyRemaining, int[] requiredPoint) {
-        super(productName, prodDescription, qtyRemaining, requiredPoint);
-        this.tier = tier;
-    }
-
-    public String getTier(int i) {
-        return tier[i];
-    }
-
-    public void setTier(String[] tier) {
-        this.tier = tier;
-    }
-    
-}
     
     //VOUCHER
     
-public class Voucher {
-    private String[] vcName;
-    private String[] vcDescription;
-    private static int[] vcRemaining;
-    private int[] requiredPoint;
 
-    public Voucher() {
-    }
-
-    public Voucher(String[] vcName, String[] vcDescription, int[] vcRemaining, int[] requiredPoint) {
-        this.vcName = vcName;
-        this.vcDescription = vcDescription;
-        this.vcRemaining = vcRemaining;
-        this.requiredPoint = requiredPoint;
-    }
-
-    public String getVcName(int i) {
-        return vcName[i];
-    }
-
-    public void setVcName(String[] vcName) {
-        this.vcName = vcName;
-    }
-
-    public String getVcDescription(int i) {
-        return vcDescription[i];
-    }
-
-    public void setVcDescription(String[] vcDescription) {
-        this.vcDescription = vcDescription;
-    }
-
-    public int getVcRemaining(int i) {
-        return vcRemaining[i];
-    }
-
-    public static void setVcRemaining(int[] vcRemaining) {
-        Voucher.vcRemaining = vcRemaining;
-    }
-
-    public int getRequiredPoint(int i) {
-        return requiredPoint[i];
-    }
-
-    public void setRequiredPoint(int[] requiredPoint) {
-        this.requiredPoint = requiredPoint;
-    }
-    
-    
-}
 }
 
 

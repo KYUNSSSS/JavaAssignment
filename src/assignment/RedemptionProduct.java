eca89c1eedb07b7d52f9ad605b12530ad0367eb8
Asus <Asus@LAPTOP-GC1TDGI5>
2024-05-01 15:29:57 +0800

    Updated

---
+++ src/assignment/RedemptionProduct.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment;
+
+/**
+ *
+ * @author Asus
+ */
+public class RedemptionProduct extends Product{
+
+    public RedemptionProduct() {
+    }
+
+    public RedemptionProduct(String[] productName, String[] productDescription, int[] qtyRemaining, int[] requiredPoint) {
+        super(productName, productDescription, qtyRemaining, requiredPoint);
+    }
+}

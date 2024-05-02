5bc7843aa35197e5782f231749b4e7f8d19a23b9
xuan <xuan@LAPTOP-5DH39D1R>
2024-05-02 08:39:31 +0800

    Merge origin/master
    # Conflicts:
    #	src/testing/Testing.java

--- src/assignment/Loyalty.java
+++ src/assignment/Loyalty.java
@@ -9,8 +9,74 @@ package assignment;
  * @author xuan
  */
 public class Loyalty {
-    //hi
-    //hihi
-    //hihih
-    //good nite
+    private String tierName;
+    private String tierDescription;
+    private int tierLevel;
+    private Customer customer;
+
+    public Loyalty() {
+        
+    }
+
+    public Loyalty(String tierName, String tierDescription, int tierLevel, Customer customer) {
+        this.tierName = tierName;
+        this.tierDescription = tierDescription;
+        this.tierLevel = tierLevel;
+        this.customer = customer;
+    }
+
+    public String getTierName() {
+        return tierName;
+    }
+
+    public void setTierName(String tierName) {
+        this.tierName = tierName;
+    }
+
+    public String getTierDescription() {
+        return tierDescription;
+    }
+
+    public void setTierDescription(String tierDescription) {
+        this.tierDescription = tierDescription;
+    }
+
+    public int getTierLevel() {
+        return tierLevel;
+    }
+
+    public void setTierLevel(int tierLevel) {
+        this.tierLevel = tierLevel;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    
+    public String displayTier() {
+        return "Your Current Tier is Tier " + tierLevel + tierName;
+    }
+    
+    public int updateTier() {
+        double amount = customer.getTotalPurchaseAmount();
+        if (amount >= 10000 ) {
+            tierLevel = 3;
+            tierName = "Platinum";
+        } else if (amount >= 1000) {
+            tierLevel = 2;
+            tierName = "Gold";
+        } else if (amount >= 100) {
+            tierLevel = 1;
+            tierName = "Silver";
+        } else {
+            tierLevel = 0;
+            tierName = "No Tier";
+        }
+        
+        return tierLevel;
+    }
 }
